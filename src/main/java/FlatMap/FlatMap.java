f0a9a00939e7ffccbd667871118894d71ad30919
JazC <jchan7713@gmail.com>
2023-12-03 11:53:59 -0800

    Add an even more nested list to flatmap

--- src/main/java/FlatMap/FlatMap.java
+++ src/main/java/FlatMap/FlatMap.java
@@ -1,10 +1,7 @@
 package FlatMap;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FlatMap {
 
@@ -21,6 +18,42 @@ public class FlatMap {
 
         System.out.println("Before flattening:\t" + nestedList);
         System.out.println("After flattening:\t" + flattenedList);
+
+        // ---------------
+        List<List<List<Integer>>> evenMoreNestedList = Arrays.asList(
+                Arrays.asList(
+                        Arrays.asList(1, 2, 3),
+                        Arrays.asList(4, 5, 6),
+                        Arrays.asList(7, 8, 9)
+                ),
+                Arrays.asList(
+                        Arrays.asList(10, 11, 12),
+                        Arrays.asList(13, 14, 15)
+                ),
+                Arrays.asList(
+                        Arrays.asList(16, 17, 18),
+                        Arrays.asList(19, 20)
+                )
+        );
+// BAD
+//        List<Integer> flattenedEvenMoreNestedList = evenMoreNestedList.stream()
+//                .flatMap(outerList -> outerList.stream().flatMap(innerList -> innerList.stream()))
+//                .toList();
+
+// BETTER
+//        List<Integer> flattenedEvenMoreNestedList = evenMoreNestedList.stream()
+//                .flatMap(outerList -> outerList.stream())
+//                .flatMap(innerList -> innerList.stream())
+//                .toList();
+
+// BEST
+        List<Integer> flattenedEvenMoreNestedList = evenMoreNestedList.stream()
+                .flatMap(List::stream)
+                .flatMap(List::stream)
+                .toList();
+
+        System.out.println("Before flattening:\t" + evenMoreNestedList);
+        System.out.println("After flattening:\t" + flattenedEvenMoreNestedList);
     }
 
 }
